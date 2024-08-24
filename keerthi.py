c7f35ec7ceea4329837f543f44db2508ac39d8d5
gmkeerthi <145956801+gmkeerthi@users.noreply.github.com>
2024-08-24 10:51:31 +0530

    Update keerthi.py

--- keerthi.py
+++ keerthi.py
@@ -7,11 +7,11 @@ Original file is located at
     https://colab.research.google.com/drive/1v8s_d8Y8OhOp9d7vpuZuO068RtU70N_S
 """
 
-import matplotlib
+# import matplotlib
 import matplotlib.pyplot as plt
 # import numpy as np
 # import pandas as pd
-import sklearn
+# import sklearn
 from sklearn import linear_model
 height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
 weight=[  8, 10 , 12, 14, 16, 18, 20]
